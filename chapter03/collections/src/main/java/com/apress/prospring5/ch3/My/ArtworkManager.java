512ebe6f262dd5cb2823186dbf7867458367aca6
MikHail854 <vip.moneta95@mail.ru>
2021-02-11 15:24:57 +0700

    collections

---
+++ chapter03/collections/src/main/java/com/apress/prospring5/ch3/My/ArtworkManager.java
@@ -0,0 +1,52 @@
+package com.apress.prospring5.ch3.My;
+
+import com.apress.prospring5.ch3.ArtworkSender;
+import com.apress.prospring5.ch3.Recipient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("artworkManager")
+public class ArtworkManager implements ArtworkSender {
+
+
+    @Autowired
+    @Qualifier("list")
+    private List list;
+
+
+    @Override
+    public void sendArtwork(String artworkPath, Recipient recipient) {
+
+    }
+
+    @Override
+    public String getFriendlyName() {
+        return null;
+    }
+
+    @Override
+    public String getShortName() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:spring/my-app-context-annotation.xml");
+        ctx.refresh();
+
+        ArtworkManager manager = (ArtworkManager) ctx.getBean("artworkManager");
+        manager.list.add("Is main");
+        manager.list.forEach(obj -> System.out.println(obj));
+        ctx.close();
+
+    }
+
+    public void addList(){
+
+    }
+
+}
