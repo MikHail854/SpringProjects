672a927acaea1558a162405526c315cf22b1b849
MikHail854 <vip.moneta95@mail.ru>
2021-02-16 18:14:22 +0700

    method-injection

---
+++ chapter03/method-injection/src/main/java/com/apress/prospring5/ch3/DemoBean.java
@@ -0,0 +1,8 @@
+package com.apress.prospring5.ch3;
+
+import java.security.Signature;
+
+public interface DemoBean {
+    Singer getMySinger();
+    void doSomething();
+}
---
+++ chapter03/method-injection/src/main/java/com/apress/prospring5/ch3/Singer.java
@@ -0,0 +1,11 @@
+package com.apress.prospring5.ch3;
+
+public class Singer {
+
+    private String lyric = "I played a dick game of chess with the salt and paper shaker";
+
+    public void sing(){
+        //System.out.println(lyric);
+    }
+
+}
