96c75e324d2fc86d1d7d4687e6f31f893a47dd04
MikHail854 <vip.moneta95@mail.ru>
2021-03-17 12:20:36 +0700

    bean-instantiation-mode

---
+++ chapter03/bean-instantiation-mode/src/main/java/com/apress/prospring5/ch3/Singleton.java
@@ -0,0 +1,20 @@
+package com.apress.prospring5.ch3;
+
+public class Singleton {
+
+    private static Singleton instance;
+
+    static {
+        instance = new Singleton();
+    }
+
+    public static Singleton getInstance() {
+        return instance;
+    }
+
+    private Singleton() {
+        //этот конструктор без аргументов требуется для того,
+        //чтобы разработчики не смогли получить экземпляр
+        //данного класса непосредственно
+    }
+}
