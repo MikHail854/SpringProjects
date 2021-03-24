1a4b81d928c32b5aef486dcfed2246b2f3f333c2
MikHail854 <vip.moneta95@mail.ru>
2021-03-24 16:47:49 +0700

    post-construct-pre-destroy

---
+++ chapter04/post-construct-pre-destroy/src/main/java/com/apress/prospring5/ch4/DestructiveBeanWithJSR250.java
@@ -0,0 +1,55 @@
+package com.apress.prospring5.ch4;
+
+
+
+import java.io.File;
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class DestructiveBeanWithJSR250 {
+
+    private File file;
+    private String filePath;
+
+    @PostConstruct
+    public void afterProperties() throws Exception{
+        System.out.println("Initializing bean");
+
+        if (filePath == null){
+            throw new IllegalArgumentException("You must specify the  filePath property of " + DestructiveBeanWithJSR250.class);
+        }
+
+        this.file = new File(filePath);
+        this.file.createNewFile();
+
+        System.out.println("File exist: " + file.exists());
+    }
+
+    @PreDestroy
+    public void destroy(){
+        System.out.println("Destroying Bean");
+
+        if (!file.delete()){
+            System.out.println("ERROR: failed to delete file.");
+        }
+
+        System.out.println("File exist: " + file.exists());
+    }
+
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public static void main(String[] args) throws Exception{
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.load("classpath:spring/app-context-annotation.xml");
+        ctx.refresh();
+
+        DestructiveBeanWithJSR250 bean = (DestructiveBeanWithJSR250) ctx.getBean("destructiveBean");
+
+        System.out.println("Calling destroy()");
+        ctx.destroy();
+        System.out.println("Called destroy()");
+    }
+}
