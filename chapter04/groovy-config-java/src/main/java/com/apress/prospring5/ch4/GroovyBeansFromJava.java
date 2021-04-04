2a4c06f3a681d321f11b91f25deda5e7522e043d
MikHail854 <vip.moneta95@mail.ru>
2021-04-04 13:57:01 +0700

    groovy-config-java

---
+++ chapter04/groovy-config-java/src/main/java/com/apress/prospring5/ch4/GroovyBeansFromJava.java
@@ -0,0 +1,16 @@
+package com.apress.prospring5.ch4;
+
+import com.apress.prospring5.ch3.xml.Singer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.GenericGroovyApplicationContext;
+
+public class GroovyBeansFromJava {
+    public static void main(String[] args) {
+        ApplicationContext ctx =
+                new GenericGroovyApplicationContext("classpath:beans.groovy");
+
+        Singer singer = ctx.getBean("singer", Singer.class);
+        System.out.println(singer);
+
+    }
+}
