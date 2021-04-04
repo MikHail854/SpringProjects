709f533a2ccd83c96bbd334419f2bde74f9b2e6d
MikHail854 <vip.moneta95@mail.ru>
2021-04-04 13:29:56 +0700

    jsr330

---
+++ chapter04/jsr330/src/main/java/com/apress/prospring5/ch4/StandardOutMessageRenderer.java
@@ -0,0 +1,35 @@
+package com.apress.prospring5.ch4;
+
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+
+@Named("massageRenderer")
+@Singleton
+public class StandardOutMessageRenderer implements MessageRenderer {
+    @Inject
+    @Named("messageProvider")
+    private MessageProvider messageProvider = null;
+
+
+    @Override
+    public void render() {
+        if (messageProvider == null) {
+            throw new RuntimeException(
+                    "You must set the property messageProvider of class: "
+                    + StandardOutMessageRenderer.class.getName());
+        }
+
+        System.out.println(messageProvider.getMessage());
+    }
+
+    @Override
+    public void setMessageProvider(MessageProvider provider) {
+        this.messageProvider = provider;
+    }
+
+    @Override
+    public MessageProvider getMessageProvider() {
+        return this.messageProvider;
+    }
+}
