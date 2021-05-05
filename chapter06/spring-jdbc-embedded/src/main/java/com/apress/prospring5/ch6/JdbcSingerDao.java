8d41d6f64ac3081765c09234789cc86494282805
MikHail854 <vip.moneta95@mail.ru>
2021-05-05 13:41:00 +0700

    exception handling

--- chapter06/spring-jdbc-embedded/src/main/java/com/apress/prospring5/ch6/JdbcSingerDao.java
+++ chapter06/spring-jdbc-embedded/src/main/java/com/apress/prospring5/ch6/JdbcSingerDao.java
@@ -2,15 +2,23 @@ package com.apress.prospring5.ch6;
 
 import org.springframework.beans.factory.BeanCreationException;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 
 public class JdbcSingerDao implements SingerDao, InitializingBean {
 
     private DataSource dataSource;
+    private JdbcTemplate jdbcTemplate;
 
     public void setDataSource(DataSource dataSource) {
         this.dataSource = dataSource;
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.setDataSource(dataSource);
+        MySQLErrorCodesTranslator errorTranslator = new MySQLErrorCodesTranslator();
+        errorTranslator.setDataSource(dataSource);
+        jdbcTemplate.setExceptionTranslator(errorTranslator);
+        this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
