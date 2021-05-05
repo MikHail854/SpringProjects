f095a2f959a558e2dcf7a1ce76130ac545352688
MikHail854 <vip.moneta95@mail.ru>
2021-05-05 20:12:12 +0700

    plain-jdbc

--- chapter06/plain-jdbc/src/main/resources/db/test-data.sql
+++ chapter06/plain-jdbc/src/main/resources/db/test-data.sql
@@ -1,7 +1,7 @@
-insert into singer (first_name, LAST_NAME, BIRTH_DATE) values ('John', 'Mayer', '1977-10-16');
-insert into singer (FIRST_NAME, LAST_NAME, BIRTH_DATE) values ('Eric', 'Clapton', '1945-03-30');
-insert into singer (first_name, last_name, birth_date) VALUES ('John', 'Butler', '1975-04-01');
+insert into musicdb_schema.singer (FIRST_NAME, LAST_NAME, BIRTH_DATE) values ('John', 'Mayer', '1977-10-16');
+insert into musicdb_schema.singer (FIRST_NAME, LAST_NAME, BIRTH_DATE) values ('Eric', 'Clapton', '1945-03-30');
+insert into musicdb_schema.singer (FIRST_NAME, LAST_NAME, birth_date) VALUES ('John', 'Butler', '1975-04-01');
 
-insert into album (SINGER_ID, TITLE, RELEASE_DATE) VALUES (1, 'The Search For Everything', '2017-01-20');
-insert into album (SINGER_ID, TITLE, RELEASE_DATE) VALUES (1, 'Battle Studies', '2009-11-17');
-insert into album (SINGER_ID, TITLE, RELEASE_DATE) VALUES (2, ' From The Cradle ', '1994-09-13');
+insert into musicdb_schema.album (SINGER_ID, TITLE, RELEASE_DATE) VALUES (1, 'The Search For Everything', '2017-01-20');
+insert into musicdb_schema.album (SINGER_ID, TITLE, RELEASE_DATE) VALUES (1, 'Battle Studies', '2009-11-17');
+insert into musicdb_schema.album (SINGER_ID, TITLE, RELEASE_DATE) VALUES (2, ' From The Cradle ', '1994-09-13')
