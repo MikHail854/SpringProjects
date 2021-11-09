5fb5ba1f7ac2a3738a71f2f025946c05bb7e37c3
MikHail854 <vip.moneta95@mail.ru>
2021-11-10 00:43:18 +0700

    Requesting the results of an ad hoc type using a constructor expression

---
+++ chapter08/jpa-crud/src/main/java/com/apress/prospring5/ch8/view/SingerSummary.java
@@ -0,0 +1,35 @@
+package com.apress.prospring5.ch8.view;
+
+import java.io.Serializable;
+
+public class SingerSummary implements Serializable {
+
+    private String firstName;
+    private String lastName;
+    private String latestAlbum;
+
+    public SingerSummary(String firstName, String lastName, String latestAlbum) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.latestAlbum = latestAlbum;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getLatestAlbum() {
+        return latestAlbum;
+    }
+
+    @Override
+    public String toString() {
+        return "First name: " + firstName +
+                ", Last name: " + lastName +
+                ", Most Recent Album: '" + latestAlbum;
+    }
+}
--- chapter08/jpa-crud/src/test/java/com/apress/prospring5/ch8/SingerSummaryJPATest.java
+++ chapter08/jpa-crud/src/test/java/com/apress/prospring5/ch8/SingerSummaryJPATest.java
@@ -1,7 +1,9 @@
 package com.apress.prospring5.ch8;
 
 import com.apress.prospring5.ch8.config.JpaConfig;
+import com.apress.prospring5.ch8.service.SingerSummaryService;
 import com.apress.prospring5.ch8.service.SingerSummaryUntypeImpl;
+import com.apress.prospring5.ch8.view.SingerSummary;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,23 +25,41 @@ public class SingerSummaryJPATest {
     private static Logger logger = LoggerFactory.getLogger(SingerSummaryJPATest.class);
     private GenericApplicationContext context;
     private SingerSummaryUntypeImpl singerSummaryUntype;
+    private SingerSummaryService singerSummaryService;
 
 
     @Before
-    public void setUp(){
+    public void setUp() {
         context = new AnnotationConfigApplicationContext(JpaConfig.class);
         singerSummaryUntype = context.getBean(SingerSummaryUntypeImpl.class);
+        singerSummaryService = context.getBean(SingerSummaryService.class);
         assertNotNull(singerSummaryUntype);
+        assertNotNull(singerSummaryService);
     }
 
     @Test
-    public void testFindAllUntype(){
+    public void testFindAll() {
+        List<SingerSummary> singers = singerSummaryService.findAll();
+        listSingerSummary(singers);
+        assertEquals(2, singers.size());
+    }
+
+    private static void listSingerSummary(List<SingerSummary> singers) {
+        logger.info("---- Listing singers summary:");
+        for (SingerSummary singer : singers) {
+            logger.info(singer.toString());
+        }
+    }
+
+
+    @Test
+    public void testFindAllUntype() {
         singerSummaryUntype.displayAllSingerSummary();
     }
 
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         context.close();
     }
 
